e61fba31743b1c7db993f2eb0a42d4a8a9614da5
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-03-28 17:15:10 +0100

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Laboratorna3
+{
+    internal class Program
+    {
+        static void maxmin(float [] arr1, int size,   ref float minel, ref float maxel)
+        {
+            for (int i = 1; i < size; i++)
+            {
+                if (arr1[i] < minel)
+                    minel = arr1[i];
+                else if (arr1[i] > maxel)
+                    maxel = arr1[i];
+
+            }
+
+        }
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Лабораторна #3, Каплаушенко М.В , ОФ-01, ФМФ");
+            int size;
+            float  minel, maxel;
+            Console.WriteLine("Введіть розмір масиву:");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            if (size == 0)
+                return;
+
+            float[] arr1 = new float[size];
+            Console.WriteLine("Введіть елементи массиву:");
+            for (int i = 0; i < size; i++)
+                arr1[i] = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введений масив: ");
+            foreach (int i in arr1)
+                Console.Write(i + " ");
+            Console.WriteLine("\n");
+
+            minel =  arr1[0];
+            maxel = arr1[0];
+
+
+
+            maxmin(arr1, size, ref maxel, ref minel);
+
+            Console.Write("Мінімальний елемент масиву = ");
+            Console.WriteLine(minel);
+
+            Console.Write("Максимальний елемент массиву = ");
+            Console.WriteLine(maxel);
+
+
+            if (minel == maxel)
+                return;
+
+
+            //float ahe(float [] mas1)
+            //{
+            //    for (int i = 0; i < size; i++)
+            //    {
+            //        if (arr1[i] == minel)
+            //            arr1[i] = maxel;
+            //        else if (arr1[i] == maxel)
+            //            arr1[i] = minel;
+            //    }
+            //    return float[size];
+            //}
+
+            for(int i = 0; i < size; i++)
+            {
+                Console.Write(arr1[i] + " ");
+            }
+
+            if (arr1[0] < arr1[1])
+            {
+
+                for (int i = 1; i < size; i++)
+                {
+                    float key = arr1[i];
+                    int j = i - 1;
+                    {
+                        while (j >= 0 && arr1[j] > key)
+                        {
+                            arr1[j + 1] = arr1[j];
+                            arr1[j] = key;
+                            j--;
+                        }
+
+                    }
+                }
+
+                Console.WriteLine("\n");
+
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(arr1[i] + " ");
+                }
+            }
+
+            else
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    float key = arr1[i];
+                    int j = i - 1;
+                    {
+                        while (j >= 0 && arr1[j] < key)
+                        {
+                            arr1[j + 1] = arr1[j];
+                            arr1[j] = key;
+                            j--;
+                        }
+
+                    }
+                }
+
+                Console.WriteLine("\n");
+
+                for (int i = 0; i < size; i++)
+                {
+                    Console.Write(arr1[i] + " ");
+                }
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Complex calculator/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace Dyachenko5
+{
+    class Program
+    {
+        public struct ComplexCal
+        {
+            private float real;
+            private float imaginary;
+
+            public ComplexCal(float real, float imaginary)
+            {
+                this.real = real;
+                this.imaginary = imaginary;
+            }
+
+            public ComplexCal(float imaginary)
+            {
+                this.real = 0;
+                this.imaginary = imaginary;
+            }
+
+            public static ComplexCal operator +(ComplexCal one, ComplexCal two)
+            {
+                return new ComplexCal(one.real + two.real, one.imaginary + two.imaginary);
+            }
+
+            public static ComplexCal operator +(ComplexCal one, float imaginary)
+            {
+                return new ComplexCal(one.real, one.imaginary + imaginary);
+            }
+
+            public static ComplexCal operator -(ComplexCal one, ComplexCal two)
+            {
+                return new ComplexCal(one.real - two.real, one.imaginary - two.imaginary);
+            }
+            public static ComplexCal operator -(ComplexCal one, float imaginary)
+            {
+                return new ComplexCal(one.real, one.imaginary - imaginary);
+            }
+
+            public static ComplexCal operator *(ComplexCal one, ComplexCal two)
+            {
+                return new ComplexCal(((one.real * two.real) - (one.imaginary * two.imaginary)), ((one.real * two.imaginary) + (two.real * one.imaginary)));
+            }
+
+            public static ComplexCal operator *(ComplexCal one, float imaginary)
+            {
+                return new ComplexCal((-one.imaginary * imaginary), (one.real * imaginary));
+            }
+
+
+            public static ComplexCal operator /(ComplexCal one, ComplexCal two)
+            {
+                if (two.real == 0 && two.imaginary == 0)
+                {
+                    Console.WriteLine("Ділення на нуль");
+                    return new ComplexCal(0, 0);
+                }
+                else
+                    return new ComplexCal((one.real * two.real + one.imaginary * two.imaginary) / (two.real * two.real + two.imaginary * two.imaginary),
+                        (two.real * one.imaginary - one.real * two.imaginary) / (two.real * two.real + two.imaginary * two.imaginary));
+
+            }
+
+            public static ComplexCal operator /(ComplexCal one, float imaginary)
+            {
+                if (imaginary == 0)
+                {
+                    Console.WriteLine("Ділення на нуль");
+                    return new ComplexCal(0, 0);
+                }
+                else
+                    return new ComplexCal((one.imaginary * imaginary) / (imaginary * imaginary),
+                        (-one.real * imaginary) / (imaginary * imaginary));
+            }
+
+
+
+            public static void Addition(ComplexCal one, ComplexCal two)
+            {
+                ComplexCal third = one + two;
+                third.Show();
+            }
+
+            public static void Addition(ComplexCal one, ComplexCal two, ComplexCal third)
+            {
+                ComplexCal fourth = one + two + third;
+                fourth.Show();
+            }
+
+            public static void Addition(ComplexCal one, float imaginary)
+            {
+                ComplexCal two = one + imaginary;
+                two.Show();
+            }
+
+            public static void Subtraction(ComplexCal one, ComplexCal two)
+            {
+                ComplexCal third = one - two;
+                third.Show();
+            }
+
+            public static void Subtraction(ComplexCal one, ComplexCal two, ComplexCal third)
+            {
+                ComplexCal fourth = one - two - third;
+                fourth.Show();
+            }
+
+            public static void Subtraction(ComplexCal one, float imaginary)
+            {
+                ComplexCal two = one - imaginary;
+                two.Show();
+            }
+
+            public static void Multiplication(ComplexCal one, ComplexCal two)
+            {
+                ComplexCal third = one * two;
+                third.Show();
+            }
+
+            public static void Multiplication(ComplexCal one, ComplexCal two, ComplexCal third)
+            {
+                ComplexCal fourth = one * two * third;
+                fourth.Show();
+            }
+
+            public static void Multiplication(ComplexCal one, float imaginary)
+            {
+                ComplexCal two = one * imaginary;
+                two.Show();
+            }
+
+            public static void Division(ComplexCal one, ComplexCal two)
+            {
+                ComplexCal third = one / two;
+                if (two.real != 0 && two.imaginary != 0)
+                    third.Show();
+            }
+
+            public static void Division(ComplexCal one, ComplexCal two, ComplexCal third)
+            {
+                ComplexCal fourth = one / two / third;
+                fourth.Show();
+            }
+            public static void Division(ComplexCal one, float imaginary)
+            {
+                ComplexCal two = one / imaginary;
+                if (imaginary != 0)
+                    two.Show();
+            }
+            public void Show()
+            {
+                Console.WriteLine($"{real}+{imaginary}i");
+            }
+
+
+        }
+
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Лабораторна робота №5" 
+                + "\nВиконала: студентка 2-го курсу" 
+                + "\nгрупи ОФ-01, ФМФ" 
+                + "\nКаплаушенко Марія");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Задані числа: ");
+
+            ComplexCal val1 = new ComplexCal(4, 4);
+            ComplexCal val2 = new ComplexCal(-2, -2);
+            ComplexCal val3 = new ComplexCal(4, 3);
+            float val4 = 2;
+            ComplexCal val5 = new ComplexCal(0, 0);
+
+            Console.WriteLine(" z1 = 4 + 4i" + 
+                "\n z2 = -2 - 2i" + 
+                "\n z3 = 4 + 3i" + 
+                "\n z4 = 2" + 
+                "\n z5 = 0"); 
+
+            Console.WriteLine();
+
+            Console.WriteLine("Дії з 2 - ма комплексними числами.");
+
+            Console.Write(" z1 + z2: ");
+            ComplexCal.Addition(val1, val2);
+
+            Console.Write(" z1 - z2: ");
+            ComplexCal.Subtraction(val1, val2);
+
+            Console.Write(" z1 * z2: ");
+            ComplexCal.Multiplication(val1, val2);
+
+            Console.Write(" z1 / z2: ");
+            ComplexCal.Division(val1, val2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Дії з 3 - ма комплексними числами.");
+
+            Console.Write(" z1 + z2 + z3: ");
+            ComplexCal.Addition(val1, val2, val3);
+
+            Console.Write(" z1 - z2 - z3: ");
+            ComplexCal.Subtraction(val1, val2, val3);
+
+            Console.Write(" z1 * z2 * z3: ");
+            ComplexCal.Multiplication(val1, val2, val3);
+
+            Console.Write(" z1 / z2 / z3: ");
+            ComplexCal.Division(val1, val2, val3);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Дії з комплексними та натуральними числами.");
+
+            Console.Write(" z2 + z4: ");
+            ComplexCal.Addition(val2, val4);
+
+            Console.Write(" z2 - z4: ");
+            ComplexCal.Subtraction(val2, val4);
+
+            Console.Write(" z2 * z4: ");
+            ComplexCal.Multiplication(val2, val4);
+
+            Console.Write(" z2 / z4: ");
+            ComplexCal.Division(val2, val4);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Дії з 0.");
+            Console.Write(" z3 / z5 : ");
+            ComplexCal.Division(val3, val5);
+
+            Console.ReadKey();
+
+        }
+    }
+}
